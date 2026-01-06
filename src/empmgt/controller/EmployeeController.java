0710fb05a6acdbbb5acd9c186e1c577830e7f6ef
tclbd <maruf.tcl@gmail.com>
2026-01-06 18:55:56 +0600

    jan06

---
+++ src/empmgt/controller/EmployeeController.java
@@ -0,0 +1,10 @@
+package empmgt.controller;
+
+import empmgt.model.Employee;
+
+public class EmployeeController {
+    
+    public void create(Employee e){
+        System.out.println(e);
+    }
+}
